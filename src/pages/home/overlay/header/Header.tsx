8d00e0be8e2e57dbe6e9f4d981e5b51f7e46883b
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2025-04-10 11:27:45 +0200

    Header finished

--- src/pages/home/overlay/header/Header.tsx
+++ src/pages/home/overlay/header/Header.tsx
@@ -1,7 +1,24 @@
-import { MdEqualizer } from 'react-icons/md'
+import { FaCodeBranch, FaGithub } from 'react-icons/fa6'
 import styles from './Header.module.scss'
+import { MdEqualizer } from 'react-icons/md'
+import { ReactElement } from 'react'
+// import { useGenreTreeStore } from '../../../../store/genreTreeStore'
+// import { FaCompactDisc } from 'react-icons/fa6'
+
+interface Link {
+  icon: ReactElement
+  href: string
+  title: string
+}
+
+const links: Link[] = [
+  { icon: <FaCodeBranch />, href: 'https://github.com/eflisback/edm-tree', title: 'Source code' },
+  { icon: <FaGithub />, href: 'https://github.com/eflisback', title: 'My GitHub profile' },
+]
 
 const Header = () => {
+  // const { selectedGenre } = useGenreTreeStore()
+
   return (
     <header className={styles.header}>
       <section className={styles.logo}>
@@ -14,7 +31,23 @@ const Header = () => {
           </span>
         </span>
       </section>
-      <section className={styles.links}>{/* TODO */}</section>
+      {/* <section className={styles.nowExploring}>
+        {selectedGenre ? (
+          <div className={styles.panel}>
+            <FaCompactDisc />
+            <span>{selectedGenre.title}</span>
+          </div>
+        ) : (
+          ''
+        )}
+      </section> */}
+      <section className={styles.links}>
+        {links.map((link, i) => (
+          <a title={link.title} key={i} href={link.href} target='_blank' rel='noopener noreferrer'>
+            {link.icon}
+          </a>
+        ))}
+      </section>
     </header>
   )
 }
